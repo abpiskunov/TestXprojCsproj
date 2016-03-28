7af67a4c82cd485f8a804b5cc4559a2bf839e18a
Anton Piskunov <antonpis@microsoft.com>
2016-03-28 14:06:30 -0700

    add more test apps

---
+++ Xproj46+1Csproj/src/ConsoleAppWithCsproj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithCsproj
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var q = new CsprojClassLibrary46.Class1();
+        }
+    }
+}
